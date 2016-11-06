a2fc3e52218a1590ad14d3f0b93957db9838af38
Flávio Brito <prof.flaviobrito@gmail.com>
2016-11-06 15:10:12 -0200

    Data - data sample functions - check_file and load_data

--- R/check_file.R
+++ R/check_file.R
@@ -1,3 +1,16 @@
-check_file <- function(){
-return(TRUE)
+check_file <- function(fpath,fname){
+  f<- paste0(fpath,"/",fname)
+  if(file.access(f,4)==0){
+    return(TRUE)
+  } else {
+    print("Please, verify the file name, file path or permissions")
+    return(FALSE)
+  }
 }
+
+###########################
+#Example
+###########################
+p <- paste0(getwd(),"/R")
+f <- "check_file2.R"
+check_file(p,f)
--- R/load_data.R
+++ R/load_data.R
@@ -1,3 +1,21 @@
-load_data <- function(){
-  return(TRUE)
+load_data <- function(fpath,fname,ftype="csv", fheader = TRUE, fstring=FALSE) {
+  f <- paste0(fpath,"/",fname)
+  if(check_file(fpath,fname)==TRUE) {
+    if(ftype=="csv") {
+          read.csv(file=f, header = fheader, stringsAsFactors = fstring)
+    } else if(ftype=="csv2") {
+      read.csv(file = f, header = fheader, stringsAsFactors = fstring, sep = ";")
+    } else if(ftype=="tab") {
+      read.table(file = f, header = fheader, stringsAsFactors = fstring, sep = "\t")
+  } else
+        return(print("Wrong option"))
+  }
 }
+
+###########################
+#Example
+###########################
+p <- paste0(getwd(),"/data")
+list.files(p)
+f1 <- "RH01_Data_B.csv"
+d <- load_data(p,f1,"csv2")
