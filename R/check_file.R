de04b38c6a473c742c13f6f8fb8ddd9a825ab00f
Flávio Brito <flaviobrito@users.noreply.github.com>
2016-11-06 20:40:29 -0200

    Function update

--- R/check_file.R
+++ R/check_file.R
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> origin/master
-check_file <- function(fpath,fname){
-  f<- paste0(fpath,"/",fname)
-  if(file.access(f,4)==0){
-    return(TRUE)
-  } else {
-    print("Please, verify the file name, file path or permissions")
-    return(FALSE)
-  }
-}
-
-###########################
-#Example
-###########################
-p <- paste0(getwd(),"/R")
-f <- "check_file2.R"
-check_file(p,f)
-<<<<<<< HEAD
-=======
-=======
-check_file <- function(){
-return(TRUE)
-}
->>>>>>> dff9b7e54607c7e3c1267a874eeb521552980748
->>>>>>> origin/master
-=======
 #' A Check File Function
 #'
 #' This function allows you to check if a file can be accessed.
@@ -51,8 +21,3 @@ check_file <- function(fpath,fname){
     return(FALSE)
   }
 }
-
-p <- paste0(getwd(),"/R")
-f <- "check_file2.R"
-check_file(p,f)
->>>>>>> origin/master
