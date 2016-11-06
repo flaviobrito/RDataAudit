cd24e35f2dbb1b2f5873be02f2bf8820f0769ca4
Flávio Brito <flaviobrito@users.noreply.github.com>
2016-11-06 20:42:20 -0200

    Function to process files

--- R/load_data.R
+++ R/load_data.R
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-load_data <- function(fpath,fname,ftype="csv", fheader = TRUE, fstring=FALSE) {
-  f <- paste0(fpath,"/",fname)
-  if(check_file(fpath,fname)==TRUE) {
-    if(ftype=="csv") {
-          read.csv(file=f, header = fheader, stringsAsFactors = fstring)
-    } else if(ftype=="csv2") {
-      read.csv(file = f, header = fheader, stringsAsFactors = fstring, sep = ";")
-    } else if(ftype=="tab") {
-      read.table(file = f, header = fheader, stringsAsFactors = fstring, sep = "\t")
-  } else
-        return(print("Wrong option"))
-  }
-}
-
-###########################
-#Example
-###########################
-p <- paste0(getwd(),"/data")
-list.files(p)
-f1 <- "RH01_Data_B.csv"
-d <- load_data(p,f1,"csv2")
-=======
 #' A Load Data Function
 #'
 #' This function allows you to load many types of data
@@ -51,9 +28,3 @@ load_data <- function(fpath,fname,ftype="csv", fheader = TRUE, fstring=FALSE) {
   } else
         return(print("Wrong option"))
   }
-}
-
-###########################
-#Example
-###########################
->>>>>>> origin/master
